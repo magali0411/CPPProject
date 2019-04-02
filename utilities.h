ba60e3d7071f9b355ca9eaa6ead4e6191c1e83e3
Magali <43990233+magali0411@users.noreply.github.com>
2019-04-02 09:14:48 +0200

    Update utilities.h

--- utilities.h
+++ utilities.h
@@ -8,4 +8,6 @@ bool isNucl(char c);
 bool StringNotSeq(const std::string &s);
 char* myStrDup (const char* s); //permet de faire une copie d'un tableau de char
 char* myStringDup (const std::string str) ;
+char revNucl(const char c);
+
 #endif
